0d989c8b1230a5a962575455c50a06f177372154
paulbogdy <jurcutpaul492001@gmail.com>
2022-11-22 17:03:10 +0200

    S

---
+++ Grammar.h
@@ -0,0 +1,12 @@
+#include <string>
+#include <unordered_set>
+
+class Grammar {
+public:
+    Grammar() = default;
+    void read_grammar(std::string file_path) {
+        
+    }
+private:
+    
+};
