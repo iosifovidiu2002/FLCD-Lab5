c8b03b027df71ad5a041fa5ba3c9564fab88fd4f
iosifovidiu2002 <iosif.ovidiu2002@gmail.com>
2022-11-22 17:11:28 +0200

    Init read_grammar

--- Grammar.h
+++ Grammar.h
@@ -2,12 +2,22 @@
 #include <vector>
 #include <string>
 #include <unordered_set>
+#include <fstream>
+#include <iostream>
 
 class Grammar {
 public:
     Grammar() = default;
     void read_grammar(std::string file_path) {
-        
+        std::ifstream file;
+
+        try{
+            file.open(file_path);
+        } catch (std::ios_base::failure& e){
+            std::cout << e.what() << '\n';
+        }
+
+        std::string line; 
     }
 private:
     std::unordered_set<std::string> terminals;
